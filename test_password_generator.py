ac308444f81df3922fc619c5600d1e0d2b0e5f1d
Aleksashka11 <59337516+Aleksashka11@users.noreply.github.com>
2022-04-16 19:43:40 +0300

    Update test_password_generator.py

--- test_password_generator.py
+++ test_password_generator.py
@@ -1,1 +1,24 @@
+from password_generator import generate, strong_password
+import unittest
+import string
 
+
+class MyTestCase(unittest.TestCase):
+    def test_strong_password(self):
+        password = strong_password()
+        bool = False
+        for char in password:
+            if char in string.punctuation:
+                punc = True
+                break
+        if any(char.islower() for char in password)\
+                and any(char.isupper() for char in password)\
+                and any(char.isdigit() for char in password)\
+                and punc == True\
+                and len(password) >= 14:
+            bool = True
+        self.assertEqual(bool, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
